952d4e8c19c2d742252276dd04d39e6321dc8bb5
Siddharth Chandrasekaran <siddharth@embedjournal.com>
2019-10-25 00:53:23 +0530

    Add tools to support export/import to/form zephyr tree

---
+++ zephyr/tools/export.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+if [ ! -d $REPO_ROOT/zephyr ]; then
+	echo "Error: this file has be in libosdp/zephyr. Did you move it?"
+	exit 1
+fi
+
+if [ "x$ZEPHYR_BASE" == "x" ]; then
+	echo "ZEPHYR_BASE is not set. source zephyr-env.sh first"
+	exit 1
+fi
+
+rm -rf $ZEPHYR_BASE/drivers/osdp/
+rm -f  $ZEPHYR_BASE/include/drivers/osdp.h
+
+mkdir -p $ZEPHYR_BASE/drivers/osdp/
+cp -rf $REPO_ROOT/src $ZEPHYR_BASE/drivers/osdp
+cp  -f $REPO_ROOT/zephyr/CMakeLists.txt $ZEPHYR_BASE/drivers/osdp
+cp  -f $REPO_ROOT/zephyr/Kconfig $ZEPHYR_BASE/drivers/osdp
+cp  -f $REPO_ROOT/zephyr/osdp_zephyr.c $ZEPHYR_BASE/drivers/osdp

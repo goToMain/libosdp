9ea2749ef7c0657adacddd75a2ea31f254b5852b
Siddharth Chandrasekaran <siddharth@embedjournal.com>
2020-07-17 21:36:17 +0530

    osdpctl: rewrite rs232.c into serial.c
    The API provided by the incumbent rs232 implementation was severely
    limiting and was causing issues with custom TTY devices names. To fix
    this, that code was fully rewritten.
    
    Signed-off-by: Siddharth Chandrasekaran <siddharth@embedjournal.com>

--- osdpctl/channel_uart.c
+++ osdpctl/channel_uart.c
@@ -7,75 +7,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "rs232.h"
+#include "serial.h"
 #include "common.h"
-
-struct channel_uart_s {
-        int port_id;
-};
 
 int channel_uart_send(void *data, uint8_t *buf, int len)
 {
-	int i;
-	struct channel_uart_s *ctx = data;
-
-	for (i = 0; i < len; i++) {
-		while (RS232_SendByte(ctx->port_id, buf[i]));
-	}
-
-	return len;
+	return serial_write(data, (unsigned char *)buf, len);
 }
 
 int channel_uart_recv(void *data, uint8_t *buf, int maxLen)
 {
-	struct channel_uart_s *ctx = data;
-
-	return RS232_PollComport(ctx->port_id, buf, maxLen);
+	return serial_read(data, (unsigned char *)buf, maxLen);
 }
 
 void channel_uart_flush(void *data)
 {
-	struct channel_uart_s *ctx = data;
-
-	RS232_flushRXTX(ctx->port_id);
+	serial_flush(data);
 }
 
 int channel_uart_setup(void **data, struct config_pd_s *c)
 {
-	struct channel_uart_s *ctx;
+	struct serial *ctx;
 
-	ctx = malloc(sizeof(struct channel_uart_s));
+	ctx = serial_open(c->channel_device, c->channel_speed, "8N1");
 	if (ctx == NULL) {
-		printf("Failed at alloc for uart channel\n");
-		return -1;
-	}
-
-	ctx->port_id = RS232_GetPortnr(c->channel_device);
-	if (ctx->port_id < 0) {
-		printf("Error: invalid uart device %s\n",
-		       c->channel_device);
-		free(ctx);
-		return -1;
-	}
-	if (RS232_OpenComport(ctx->port_id, c->channel_speed, "8N1", 0)) {
 		printf("Error: failed to open device %s\n",
 		       c->channel_device);
-		free(ctx);
 		return -1;
 	}
 	*data = (void *)ctx;
-
 	return 0;
 }
 
 void channel_uart_teardown(void *data)
 {
-	struct channel_uart_s *ctx = data;
-
-	if (ctx != NULL) {
-		RS232_CloseComport(ctx->port_id);
-		free(ctx);
-	}
+	serial_close(data);
 }
 
 struct channel_ops_s channel_uart = {

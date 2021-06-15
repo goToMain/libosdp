b804fc0cfeaedfbb33747eaa14f7045076a7d523
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2021-06-15 23:09:24 +0200

    Add clan-foramt-diff-check.py and hook it do GitHub actions CI
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

---
+++ scripts/clang-format-check.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#  Copyright (c) 2021 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
+#
+#  SPDX-License-Identifier: Apache-2.0
+#
+
+refs_str="$(git rev-list --parents -n1 HEAD)"; refs=($refs_str)
+
+merge_head=${refs[0]}
+base_head=${refs[1]}
+branch_head=${refs[2]}
+
+files=$(git diff --name-only ${base_head} | grep -E ".*\.(cpp|cc|c\+\+|cxx|c|h|hpp)$")
+if [ -z "${files}" ]; then
+       echo "No source code to check for formatting."
+       exit 0
+fi
+
+diff=$(git diff -U0 ${base_head} -- ${files} | ./scripts/clang-format-diff.py -p1)
+
+if [ -z "${diff}" ]; then
+       echo "All source code in PR properly formatted."
+       exit 0
+fi
+
+echo -e "\nFound formatting errors!\n\n"
+
+echo "${diff}"
+
+echo "\n\nRun 'clang-format --style=file -i FILE_YOU_MODIFED' fix these issues!"
+exit 1

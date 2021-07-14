923c9603e1864c9a450724e948a0148c023ab251
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2021-07-14 22:37:12 +0200

    libosdp: Discard Secure Channel if a KEYSET is ACKed in plaintext
    HID readers are responding to a KEYSET command with an ACK in plaintext
    instead of using the current session keys to acknowledge this command
    (which is the reasonable thing to do as the command itself was received
    encrypted with the old key). Since the spec doesn't say anything about
    this, both methods are technically correct.
    
    Make changes to CP so it allows ACKs in plaintext for KEYSET command in
    particular and make the PD implementation do this too as it makes the
    code look cleaner there (perhaps HID did it for the same reasons :D).
    
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- src/osdp_pd.c
+++ src/osdp_pd.c
@@ -560,6 +560,7 @@ static int pd_decode_command(struct osdp_pd *pd, uint8_t *buf, int len)
 		}
 		CLEAR_FLAG(pd, PD_FLAG_SC_USE_SCBKD);
 		CLEAR_FLAG(pd, OSDP_FLAG_INSTALL_MODE);
+		sc_deactivate(pd);
 		pd->reply_id = REPLY_ACK;
 		ret = OSDP_PD_ERR_NONE;
 		break;

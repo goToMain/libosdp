goToMain/libosdp

commit 4028358519ccb7b7f515ba3e2f59651e55d0fd5a

phy: Allow sequence repeat packets
OSDP specification supports command retires by allowing the CP to resend
the same command without incrementing the sequence number which prompts
to PD to resend the last response. One major use case for this feature
is to work around RS485 channel errors where the CP find the received
packet to have checksum errors.

Handling this feature in the way the specification intended, makes the
code look ugly, complicates things or wastes resources. Instead, we will
just ignore such non-movement in packet sequence and re-process the
packet all over again as it it was the first time we are seeing it.
Since this is an unhappy case, keeping it simple is more important than
performance.

Fixes: #41
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_phy.c
@@ -101,13 +101,19 @@ uint8_t osdp_compute_checksum(uint8_t *msg, int length)
 	return checksum;
 }
 
+/**
+ * do_inc can be 0, 1, or -1. Non-zero values control which direction the
+ * sequence progresses; i.e. moving forward +1 or backward with -1.
+ */
 static int osdp_phy_get_seq_number(struct osdp_pd *pd, int do_inc)
 {
 	/* pd->seq_num is set to -1 to reset phy cmd state */
 	if (do_inc) {
-		pd->seq_number += 1;
+		pd->seq_number += do_inc;
 		if (pd->seq_number > 3) {
 			pd->seq_number = 1;
+		} else if (pd->seq_number < 1) {
+			pd->seq_number = 3;
 		}
 	}
 	return pd->seq_number & PKT_CONTROL_SQN;
@@ -439,13 +445,14 @@ static int phy_check_packet(struct osdp_pd *pd, uint8_t *buf, int pkt_len)
 		}
 		if (comp == pd->seq_number) {
 			/**
-			 * TODO: PD must resend the last response if CP send the
-			 * same sequence number again.
+			 * Sometimes, a CP re-sends the same command without
+			 * incrementing the sequence number. To handle such cases,
+			 * we will move the sequence back one step (with do_inc
+			 * set to -1) and then process the packet all over again
+			 * as if it was the first time we are seeing it.
 			 */
-			LOG_ERR("seq-repeat/reply-resend not supported!");
-			pd->reply_id = REPLY_NAK;
-			pd->ephemeral_data[0] = OSDP_PD_NAK_SEQ_NUM;
-			return OSDP_ERR_PKT_NACK;
+			osdp_phy_get_seq_number(pd, -1);
+			LOG_INF("received a sequence repeat packet!");
 		}
 	} else {
 		if (comp == 0) {

commit 0895b22f6b35e192675b005cab597d30cafc0703

Fix sequence reversing issue during phy_reset()
Commit 4028358 ("phy: Allow sequence repeat packets") attempted to allow
sequence repeats by moving the sequence numbers backwards but that
inadvertently broke the initial case: -1 to 0 transitions during a
phy reset. Lucky for us, our unit test suit caught it :)

Fix this issue by decoupling the sequence reversing from the advancing
method.

Fixes: 4028358519ccb7b7f515ba3e2f59651e55d0fd5a
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_phy.c
@@ -101,19 +101,13 @@ uint8_t osdp_compute_checksum(uint8_t *msg, int length)
 	return checksum;
 }
 
-/**
- * do_inc can be 0, 1, or -1. Non-zero values control which direction the
- * sequence progresses; i.e. moving forward +1 or backward with -1.
- */
 static int osdp_phy_get_seq_number(struct osdp_pd *pd, int do_inc)
 {
 	/* pd->seq_num is set to -1 to reset phy cmd state */
 	if (do_inc) {
-		pd->seq_number += do_inc;
+		pd->seq_number += 1;
 		if (pd->seq_number > 3) {
 			pd->seq_number = 1;
-		} else if (pd->seq_number < 1) {
-			pd->seq_number = 3;
 		}
 	}
 	return pd->seq_number & PKT_CONTROL_SQN;
@@ -443,7 +437,7 @@ static int phy_check_packet(struct osdp_pd *pd, uint8_t *buf, int pkt_len)
 			pd->seq_number = -1;
 			sc_deactivate(pd);
 		}
-		if (comp == pd->seq_number) {
+		else if (comp == pd->seq_number) {
 			/**
 			 * Sometimes, a CP re-sends the same command without
 			 * incrementing the sequence number. To handle such cases,
@@ -451,7 +445,7 @@ static int phy_check_packet(struct osdp_pd *pd, uint8_t *buf, int pkt_len)
 			 * set to -1) and then process the packet all over again
 			 * as if it was the first time we are seeing it.
 			 */
-			osdp_phy_get_seq_number(pd, -1);
+			pd->seq_number -= 1;
 			LOG_INF("received a sequence repeat packet!");
 		}
 	} else {
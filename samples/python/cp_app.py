7c48171fc537b15eeab996ddea518de9e0a3a63e
Andrew Stucky <andrew.david.stucky@gmail.com>
2023-05-19 16:32:15 -0400

    fix: updates sample to use sc_status() method.

--- samples/python/cp_app.py
+++ samples/python/cp_app.py
@@ -102,7 +102,7 @@ def main():
     while True:
         cp.refresh()
 
-        if (count % 100) == 99 and cp.is_sc_active(PD_0):
+        if (count % 100) == 99 and bool(cp.sc_status() & 1 << PD_0):
             # send a random command to the PD_0
             r = random.randint(PD_0, len(commands)-1)
             cp.send_command(PD_0, commands[r])

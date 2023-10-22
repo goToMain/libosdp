goToMain/libosdp

commit c9f3f981a4b9fdbc0c4db8d54f3b95c322cf7909

libopsdp: Update to new logger API
utils/logger.h was providing some really messy API for the logger. This
patch integrates the new reworked API into libosdp. This is an
experimental change with a lot of potential change/enhancements in
future.

Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

include/osdp.h
@@ -967,6 +967,7 @@ typedef int (*osdp_log_puts_fn_t)(const char *msg);
 /**
  * @brief Configure OSDP Logging.
  *
+ * @param name A soft name for this module; will appear in all the log lines.
  * @param log_level OSDP log levels of type `enum osdp_log_level_e`. Default is
  *                  LOG_INFO.
  * @param puts_fn A puts() like function that will be invoked to write the log
@@ -975,7 +976,8 @@ typedef int (*osdp_log_puts_fn_t)(const char *msg);
  *                definition to see the behavioral expectations. When this is
  *                set to NULL, LibOSDP will log to stderr.
  */
-void osdp_logger_init(int log_level, osdp_log_puts_fn_t puts_fn);
+void osdp_logger_init(const char *name, int log_level,
+		      osdp_log_puts_fn_t puts_fn);
 
 /**
  * @brief Get LibOSDP version as a `const char *`. Used in diagnostics.

include/osdp.hpp
@@ -16,9 +16,9 @@ class Common {
 public:
 	Common() {}
 
-	void logger_init(int log_level, osdp_log_puts_fn_t puts_fn)
+	void logger_init(const char *name, int log_level, osdp_log_puts_fn_t puts_fn)
 	{
-		osdp_logger_init(log_level, puts_fn);
+		osdp_logger_init(name, log_level, puts_fn);
 	}
 
 	const char *get_version()

osdpctl/cmd_start.c
@@ -317,7 +317,7 @@ int cmd_handler_start(int argc, char *argv[], void *data)
 		info->scbk = NULL;
 	}
 
-	osdp_logger_init(c->log_level, NULL);
+	osdp_logger_init("osdp::cp", c->log_level, NULL);
 
 	if (c->mode == CONFIG_MODE_CP) {
 		c->cp_ctx = osdp_cp_setup2(c->num_pd, info_arr);

python/pyosdp_base.c
@@ -214,7 +214,7 @@ static PyObject *pyosdp_set_loglevel(pyosdp_base_t *self, PyObject *args)
 		return NULL;
 	}
 
-	osdp_logger_init(log_level, NULL);
+	osdp_logger_init("pyosdp", log_level, NULL);
 
 	Py_RETURN_NONE;
 }
@@ -263,7 +263,7 @@ static int pyosdp_base_tp_init(pyosdp_base_t *self, PyObject *args, PyObject *kw
 
 	channel_manager_init(&self->channel_manager);
 
-	osdp_logger_init(OSDP_LOG_INFO, NULL);
+	osdp_logger_init("pyosdp", OSDP_LOG_INFO, NULL);
 
 	return 0;
 }

samples/c/cp_app.c
@@ -53,7 +53,7 @@ int main()
 {
 	osdp_t *ctx;
 
-	osdp_logger_init(OSDP_LOG_DEBUG, NULL);
+	osdp_logger_init("osdp::cp", OSDP_LOG_DEBUG, NULL);
 
 	ctx = osdp_cp_setup2(1, pd_info);
 	if (ctx == NULL) {

samples/c/pd_app.c
@@ -85,7 +85,7 @@ int main()
 {
 	osdp_t *ctx;
 
-	osdp_logger_init(OSDP_LOG_DEBUG, NULL);
+	osdp_logger_init("osdp::pd", OSDP_LOG_DEBUG, NULL);
 
 	ctx = osdp_pd_setup(&info_pd);
 	if (ctx == NULL) {

src/osdp_common.c
@@ -15,12 +15,6 @@
 
 #include "osdp_common.h"
 
-#ifdef CONFIG_DISABLE_PRETTY_LOGGING
-LOGGER_DEFINE(osdp, LOG_INFO, LOGGER_FLAG_NO_COLORS);
-#else
-LOGGER_DEFINE(osdp, LOG_INFO, LOGGER_FLAG_NONE);
-#endif
-
 uint16_t crc16_itu_t(uint16_t seed, const uint8_t *src, size_t len)
 {
 	for (; len > 0; len--) {
@@ -198,18 +192,21 @@ int osdp_rb_pop_buf(struct osdp_rb *p, uint8_t *buf, int max_len)
 /* --- Exported Methods --- */
 
 OSDP_EXPORT
-void osdp_logger_init(int log_level, osdp_log_puts_fn_t log_fn)
+void osdp_logger_init(const char *name, int log_level,
+		      osdp_log_puts_fn_t log_fn)
 {
-	logger_t *ctx = get_log_ctx(osdp);
+	logger_t ctx;
+	FILE *file = NULL;
+	int flags = LOGGER_FLAG_NONE;
 
-	logger_set_log_level(ctx, log_level);
-	if (log_fn) {
-		logger_set_put_fn(ctx, log_fn);
-		logger_set_file(ctx, NULL);
-	} else {
-		logger_set_put_fn(ctx, NULL);
-		logger_set_file(ctx, stderr);
-	}
+#ifdef CONFIG_DISABLE_PRETTY_LOGGING
+	flags |= LOGGER_FLAG_NO_COLORS;
+#endif
+	if (!log_fn)
+		file = stderr;
+
+	logger_init(&ctx, log_level, name, REPO_ROOT, log_fn, file, flags);
+	logger_set_default(&ctx); /* Mark this config as logging default */
 }
 
 OSDP_EXPORT

tests/unit-tests/test-cp-fsm.c
@@ -94,7 +94,7 @@ int test_cp_fsm_setup(struct test *t)
 		.channel.flush = NULL,
 		.scbk = NULL,
 	};
-	osdp_logger_init(t->loglevel, NULL);
+	osdp_logger_init("osdp::cp", t->loglevel, NULL);
 	struct osdp *ctx = (struct osdp *)osdp_cp_setup2(1, &info);
 	if (ctx == NULL) {
 		printf("   init failed!\n");

commit aaa303b0ce0d2b79bd2e98061bf6084f1ed6dfb3

libosdp: Fix stable API change violation
Commit c9f3f98 broke API backward compatibility without going through the
stable API change process. Fix this issue by reintroducing the removed
method and aliasing it to the new one.

Related-to: #139
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

include/osdp.h
@@ -976,8 +976,23 @@ typedef int (*osdp_log_puts_fn_t)(const char *msg);
  *                definition to see the behavioral expectations. When this is
  *                set to NULL, LibOSDP will log to stderr.
  */
-void osdp_logger_init(const char *name, int log_level,
+void osdp_logger_init3(const char *name, int log_level,
 		      osdp_log_puts_fn_t puts_fn);
+
+/**
+ * @brief Configure OSDP Logging (deprecated). Provided for backward
+ *        compatiblity. Use osdp_logger_init3 instead.
+ *
+ * @param log_level OSDP log levels of type `enum osdp_log_level_e`. Default is
+ *                  LOG_INFO.
+ * @param puts_fn A puts() like function that will be invoked to write the log
+ *                buffer. Can be handy if you want to log to file on a UART
+ *                device without putchar redirection. See `osdp_log_puts_fn_t`
+ *                definition to see the behavioral expectations. When this is
+ *                set to NULL, LibOSDP will log to stderr.
+ */
+void osdp_logger_init(int log_level, osdp_log_puts_fn_t puts_fn)
+	__attribute__((deprecated("Use osdp_logger_init3 instead!")));
 
 /**
  * @brief Get LibOSDP version as a `const char *`. Used in diagnostics.

include/osdp.hpp
@@ -21,7 +21,7 @@ public:
 
 	void logger_init(const char *name, int log_level, osdp_log_puts_fn_t puts_fn)
 	{
-		osdp_logger_init(name, log_level, puts_fn);
+		osdp_logger_init3(name, log_level, puts_fn);
 	}
 
 	const char *get_version()

osdpctl/cmd_start.c
@@ -317,7 +317,7 @@ int cmd_handler_start(int argc, char *argv[], void *data)
 		info->scbk = NULL;
 	}
 
-	osdp_logger_init("osdp::cp", c->log_level, NULL);
+	osdp_logger_init3("osdp::cp", c->log_level, NULL);
 
 	if (c->mode == CONFIG_MODE_CP) {
 		c->cp_ctx = osdp_cp_setup2(c->num_pd, info_arr);

python/pyosdp_base.c
@@ -214,7 +214,7 @@ static PyObject *pyosdp_set_loglevel(pyosdp_base_t *self, PyObject *args)
 		return NULL;
 	}
 
-	osdp_logger_init("pyosdp", log_level, NULL);
+	osdp_logger_init3("pyosdp", log_level, NULL);
 
 	Py_RETURN_NONE;
 }
@@ -263,7 +263,7 @@ static int pyosdp_base_tp_init(pyosdp_base_t *self, PyObject *args, PyObject *kw
 
 	channel_manager_init(&self->channel_manager);
 
-	osdp_logger_init("pyosdp", OSDP_LOG_INFO, NULL);
+	osdp_logger_init3("pyosdp", OSDP_LOG_INFO, NULL);
 
 	return 0;
 }

samples/c/cp_app.c
@@ -53,7 +53,7 @@ int main()
 {
 	osdp_t *ctx;
 
-	osdp_logger_init("osdp::cp", OSDP_LOG_DEBUG, NULL);
+	osdp_logger_init3("osdp::cp", OSDP_LOG_DEBUG, NULL);
 
 	ctx = osdp_cp_setup2(1, pd_info);
 	if (ctx == NULL) {

samples/c/pd_app.c
@@ -85,7 +85,7 @@ int main()
 {
 	osdp_t *ctx;
 
-	osdp_logger_init("osdp::pd", OSDP_LOG_DEBUG, NULL);
+	osdp_logger_init3("osdp::pd", OSDP_LOG_DEBUG, NULL);
 
 	ctx = osdp_pd_setup(&info_pd);
 	if (ctx == NULL) {

src/osdp_common.c
@@ -192,7 +192,7 @@ int osdp_rb_pop_buf(struct osdp_rb *p, uint8_t *buf, int max_len)
 /* --- Exported Methods --- */
 
 OSDP_EXPORT
-void osdp_logger_init(const char *name, int log_level,
+void osdp_logger_init3(const char *name, int log_level,
 		      osdp_log_puts_fn_t log_fn)
 {
 	logger_t ctx;
@@ -207,6 +207,12 @@ void osdp_logger_init(const char *name, int log_level,
 
 	logger_init(&ctx, log_level, name, REPO_ROOT, log_fn, file, flags);
 	logger_set_default(&ctx); /* Mark this config as logging default */
+}
+
+OSDP_EXPORT
+void osdp_logger_init(int log_level, osdp_log_puts_fn_t log_fn)
+{
+	osdp_logger_init3("osdp", log_level, log_fn);
 }
 
 OSDP_EXPORT

tests/unit-tests/test-cp-fsm.c
@@ -94,7 +94,7 @@ int test_cp_fsm_setup(struct test *t)
 		.channel.flush = NULL,
 		.scbk = NULL,
 	};
-	osdp_logger_init("osdp::cp", t->loglevel, NULL);
+	osdp_logger_init3("osdp::cp", t->loglevel, NULL);
 	struct osdp *ctx = (struct osdp *)osdp_cp_setup2(1, &info);
 	if (ctx == NULL) {
 		printf("   init failed!\n");

tests/unit-tests/test-cp-phy-fsm.c
@@ -97,7 +97,7 @@ int test_cp_phy_fsm_setup(struct test *t)
 		.channel.flush = NULL,
 		.scbk = NULL,
 	};
-	osdp_logger_init("osdp::cp", t->loglevel, NULL);
+	osdp_logger_init3("osdp::cp", t->loglevel, NULL);
 	struct osdp *ctx = (struct osdp *)osdp_cp_setup2(1, &info);
 	if (ctx == NULL) {
 		printf("   init failed!\n");

tests/unit-tests/test-cp-phy.c
@@ -147,7 +147,7 @@ int test_cp_phy_setup(struct test *t)
 		.channel.flush = NULL,
 		.scbk = NULL,
 	};
-	osdp_logger_init("osdp::cp", t->loglevel, NULL);
+	osdp_logger_init3("osdp::cp", t->loglevel, NULL);
 	struct osdp *ctx = (struct osdp *)osdp_cp_setup2(1, &info);
 	if (ctx == NULL) {
 		printf(SUB_1 "init failed!\n");

tests/unit-tests/test.c
@@ -195,7 +195,7 @@ int test_mock_pd_receive(void *data, uint8_t *buf, int len)
 
 int test_setup_devices(struct test *t, osdp_t **cp, osdp_t **pd)
 {
-	osdp_logger_init("osdp", t->loglevel, NULL);
+	osdp_logger_init3("osdp", t->loglevel, NULL);
 
 	uint8_t scbk[16] = {
 		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
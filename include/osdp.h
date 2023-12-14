goToMain/libosdp

commit 0bde480c89fde2b317f2fad712333a3211b9b7eb

libosdp: Remove deprecated methods tree-wide
In preparation of v3.0.x, let's drop support for all deprecated methods.
Since this is a major release, we don't have to worry about API/ABI
breakage.

Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

include/osdp.h
@@ -816,27 +816,6 @@ int osdp_pd_flush_events(osdp_t *ctx);
 /* ------------------------------- */
 
 /**
- * @brief This method is used to setup a device in CP mode. Application must
- * store the returned context pointer and pass it back to all OSDP functions
- * intact.
- *
- * @param num_pd Number of PDs connected to this CP. The `osdp_pd_info_t *` is
- *        treated as an array of length num_pd.
- * @param info Pointer to info struct populated by application.
- * @param master_key 16 byte Master Key from which the SCBK (Secure Channel Base
- *        Key) is generated. If this field is NULL, then secure channel is
- *        disabled.
- *
- *        Note: Master key based SCBK derivation is discouraged. Pass SCBK for
- *        each connected PD in osdp_pd_info_t::scbk.
- *
- * @retval OSDP Context on success
- * @retval NULL on errors
- */
-osdp_t *osdp_cp_setup(int num_pd, osdp_pd_info_t *info, uint8_t *master_key)
-	__attribute__((deprecated("Use osdp_cp_setup2 instead!")));
-
-/**
  * @brief Same as osdp_cp_setup; master_key is NULL here to favour the  more
  * secure individual SCBK approach (passed via osdp_pd_info_t).
  *
@@ -847,7 +826,7 @@ osdp_t *osdp_cp_setup(int num_pd, osdp_pd_info_t *info, uint8_t *master_key)
  * @retval OSDP Context on success
  * @retval NULL on errors
  */
-osdp_t *osdp_cp_setup2(int num_pd, osdp_pd_info_t *info);
+osdp_t *osdp_cp_setup(int num_pd, osdp_pd_info_t *info);
 
 /**
  * @brief Periodic refresh method. Must be called by the application at least
@@ -992,16 +971,8 @@ typedef void (*osdp_log_callback_fn_t)(int log_level, const char *file,
  * Note: This function has to be called before osdp_{cp,pd}_setup(). Otherwise
  *       it will be ignored.
  */
-void osdp_logger_init3(const char *name, int log_level,
+void osdp_logger_init(const char *name, int log_level,
 		      osdp_log_puts_fn_t puts_fn);
-
-/**
- * @brief This macro alias Provided for API compatiblity for existing users who
- * using the old osdp_logger_init() API. For description of parameters and use,
- * see osdp_logger_init3 documentation.
- */
-#define osdp_logger_init(log_level, log_fn) \
-	osdp_logger_init3("osdp", log_level, log_fn)
 
 /**
  * @brief A callback function that gets called when LibOSDP wants to emit a log
@@ -1051,22 +1022,6 @@ void osdp_get_status_mask(osdp_t *ctx, uint8_t *bitmask);
  *                (num_pds + 7 / 8).
  */
 void osdp_get_sc_status_mask(osdp_t *ctx, uint8_t *bitmask);
-
-/**
- * @brief Set osdp_command_complete_callback_t to subscribe to osdp command or
- * event completion events. This can be used to perform post command actions
- * such as changing the baud rate of the underlying channel after a COMSET
- * command was acknowledged/issued by a peer.
- *
- * @param ctx OSDP context
- * @param cb Callback to be invoked when a command is completed.
- * @param arg A pointer that will be passed as the first argument of `cb`
- */
-void osdp_set_command_complete_callback(osdp_t *ctx,
-					osdp_command_complete_callback_t cb,
-					void *arg)
-	__attribute__((deprecated("Use of this API is discouraged. A future "
-				  "version will remove it.")));
 
 /**
  * @brief OSDP File operations struct that needs to be filled by the CP/PD

src/osdp_common.c
@@ -192,7 +192,7 @@ int osdp_rb_pop_buf(struct osdp_rb *p, uint8_t *buf, int max_len)
 /* --- Exported Methods --- */
 
 OSDP_EXPORT
-void osdp_logger_init3(const char *name, int log_level,
+void osdp_logger_init(const char *name, int log_level,
 		       osdp_log_puts_fn_t log_fn)
 {
 	logger_t ctx;

src/osdp_cp.c
@@ -1314,18 +1314,8 @@ error:
 
 /* --- Exported Methods --- */
 
-OSDP_DEPRECATED_EXPORT
-osdp_t *osdp_cp_setup(int num_pd, osdp_pd_info_t *info_list, uint8_t *master_key)
-{
-	assert(info_list);
-	assert(num_pd > 0);
-	assert(num_pd <= OSDP_PD_MAX);
-
-	return (osdp_t *)__cp_setup(num_pd, info_list, master_key);
-}
-
 OSDP_EXPORT
-osdp_t *osdp_cp_setup2(int num_pd, osdp_pd_info_t *info_list)
+osdp_t *osdp_cp_setup(int num_pd, osdp_pd_info_t *info_list)
 {
 	assert(info_list);
 	assert(num_pd > 0);

include/osdp.hpp
@@ -19,9 +19,10 @@ class Common {
 public:
 	Common() {}
 
-	void logger_init(const char *name, int log_level, osdp_log_puts_fn_t puts_fn)
+	void logger_init(const char *name, int log_level,
+			 osdp_log_puts_fn_t puts_fn)
 	{
-		osdp_logger_init3(name, log_level, puts_fn);
+		osdp_logger_init(name, log_level, puts_fn);
 	}
 
 	const char *get_version()
@@ -42,11 +43,6 @@ public:
 	void get_sc_status_mask(uint8_t *bitmask)
 	{
 		osdp_get_sc_status_mask(_ctx, bitmask);
-	}
-
-	void set_command_complete_callback(osdp_command_complete_callback_t cb, void *arg)
-	{
-		osdp_set_command_complete_callback(_ctx, cb, arg);
 	}
 
 	int file_register_ops(int pd, struct osdp_file_ops *ops)
@@ -79,7 +75,7 @@ public:
 
 	bool setup(int num_pd, osdp_pd_info_t *info)
 	{
-		_ctx = osdp_cp_setup2(num_pd, info);
+		_ctx = osdp_cp_setup(num_pd, info);
 		return _ctx != nullptr;
 	}
 

python/pyosdp_base.c
@@ -84,13 +84,13 @@ int pyosdp_fops_write(void *arg, const void *buf, int size, int offset)
 	return written;
 }
 
-void pyosdp_fops_close(void *arg)
+int  pyosdp_fops_close(void *arg)
 {
 	pyosdp_base_t *self = arg;
 	PyObject *arglist, *result;
 
 	if (!self->fops.close_cb)
-		return;
+		return 0;
 
 	arglist = Py_BuildValue("(I)", self->file_id);
 
@@ -98,6 +98,7 @@ void pyosdp_fops_close(void *arg)
 
 	Py_XDECREF(result);
 	Py_DECREF(arglist);
+	return 0;
 }
 
 #define pyosdp_file_tx_status_doc                                                     \
@@ -214,7 +215,7 @@ static PyObject *pyosdp_set_loglevel(pyosdp_base_t *self, PyObject *args)
 		return NULL;
 	}
 
-	osdp_logger_init3("pyosdp", log_level, NULL);
+	osdp_logger_init("pyosdp", log_level, NULL);
 
 	Py_RETURN_NONE;
 }
@@ -263,7 +264,7 @@ static int pyosdp_base_tp_init(pyosdp_base_t *self, PyObject *args, PyObject *kw
 
 	channel_manager_init(&self->channel_manager);
 
-	osdp_logger_init3("pyosdp", OSDP_LOG_INFO, NULL);
+	osdp_logger_init("pyosdp", OSDP_LOG_INFO, NULL);
 
 	return 0;
 }

python/pyosdp_cp.c
@@ -296,7 +296,7 @@ static int pyosdp_cp_tp_init(pyosdp_cp_t *self, PyObject *args, PyObject *kwargs
 			    &info->channel.recv, &info->channel.flush);
 	}
 
-	ctx = osdp_cp_setup2(self->num_pd, info_list);
+	ctx = osdp_cp_setup(self->num_pd, info_list);
 	if (ctx == NULL) {
 		PyErr_SetString(PyExc_Exception, "failed to setup CP");
 		goto error;

rust/src/cp.rs
@@ -137,12 +137,12 @@ impl ControlPanel {
         Ok((size, offset))
     }
 
-    pub fn get_version(&mut self) -> String {
+    pub fn get_version(&self) -> String {
         let s = unsafe { crate::osdp_get_version() };
         crate::common::cstr_to_string(s)
     }
 
-    pub fn get_source_info(&mut self) -> String {
+    pub fn get_source_info(&self) -> String {
         let s = unsafe { crate::osdp_get_source_info() };
         crate::common::cstr_to_string(s)
     }

rust/src/pd.rs
@@ -105,12 +105,12 @@ impl PeripheralDevice {
         Ok((size, offset))
     }
 
-    pub fn get_version(&mut self) -> String {
+    pub fn get_version(&self) -> String {
         let s = unsafe { crate::osdp_get_version() };
         crate::common::cstr_to_string(s)
     }
 
-    pub fn get_source_info(&mut self) -> String {
+    pub fn get_source_info(&self) -> String {
         let s = unsafe { crate::osdp_get_source_info() };
         crate::common::cstr_to_string(s)
     }

commit f52cbcd007c9afa989fcc029cd0e250857fb7d96

libosdp: API: Remove osdp_set_command_complete_callback
This API was marked deprecated and removed in the past but turns out the
removal was only superficial and missed removing most of the
implementation.

Fixes: 0bde480c89fde2b317f2fad712333a3211b9b7eb
Reported-by: #151
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

include/osdp.h
@@ -751,17 +751,6 @@ typedef int (*pd_command_callback_t)(void *arg, struct osdp_cmd *cmd);
  */
 typedef int (*cp_event_callback_t)(void *arg, int pd, struct osdp_event *ev);
 
-/**
- * @brief Callback for command completion event callbacks. After it has
- * been registered with `osdp_set_command_complete_callback()` this method is
- * invoked after a command has been processed successfully in CP and PD sides.
- *
- * @param arg pointer that was passed to the arg param of
- * `osdp_set_command_complete_callback`.
- * @param id OSDP command ID (Note: this is not `enum osdp_cmd_e`)
- */
-typedef void (*osdp_command_complete_callback_t)(void *arg, int id);
-
 /* ------------------------------- */
 /*            PD Methods           */
 /* ------------------------------- */

src/osdp_common.c
@@ -282,14 +282,3 @@ void osdp_get_status_mask(const osdp_t *ctx, uint8_t *bitmask)
 		}
 	}
 }
-
-OSDP_EXPORT
-void osdp_set_command_complete_callback(osdp_t *ctx,
-					osdp_command_complete_callback_t cb,
-					void *arg)
-{
-	input_check(ctx);
-
-	TO_OSDP(ctx)->command_complete_callback = cb;
-	TO_OSDP(ctx)->command_complete_callback_arg = arg;
-}

src/osdp_common.h
@@ -356,13 +356,6 @@ struct osdp {
 	int *channel_lock;     /* array of length NUM_PD() to lock a channel */
 	uint8_t sc_master_key[16]; /* Secure Channel master key (deprecated) */
 
-	/**
-	 * OSDP defined command complete callback subscription with opaque arg
-	 * pointer as passed by app
-	 **/
-	void *command_complete_callback_arg;
-	osdp_command_complete_callback_t command_complete_callback;
-
 	/* CP event callback to app with opaque arg pointer as passed by app */
 	void *event_callback_arg;
 	cp_event_callback_t event_callback;

src/osdp_cp.c
@@ -792,7 +792,7 @@ static inline bool cp_sc_should_retry(struct osdp_pd *pd)
 
 int cp_translate_cmd(struct osdp_pd *pd, struct osdp_cmd *cmd)
 {
-	int cmd_id;
+	int cmd_id = -1;
 
 
 	if (cmd->id >= CMD_POLL) {
@@ -857,7 +857,6 @@ static int cp_phy_state_update(struct osdp_pd *pd)
 	int64_t elapsed;
 	int rc, ret = OSDP_CP_ERR_CAN_YIELD;
 	struct osdp_cmd *cmd = NULL;
-	struct osdp *ctx = pd_to_osdp(pd);
 
 	switch (pd->phy_state) {
 	case OSDP_CP_PHY_STATE_WAIT:
@@ -901,10 +900,6 @@ static int cp_phy_state_update(struct osdp_pd *pd)
 	case OSDP_CP_PHY_STATE_REPLY_WAIT:
 		rc = cp_process_reply(pd);
 		if (rc == OSDP_CP_ERR_NONE) {
-			if (ctx->command_complete_callback) {
-				ctx->command_complete_callback(ctx->command_complete_callback_arg,
-							       pd->cmd_id);
-			}
 			if (sc_is_active(pd)) {
 				pd->sc_tstamp = osdp_millis_now();
 			}

src/osdp_pd.c
@@ -988,7 +988,6 @@ static inline void pd_error_reset(struct osdp_pd *pd)
 static void osdp_pd_update(struct osdp_pd *pd)
 {
 	int ret;
-	struct osdp *ctx = pd_to_osdp(pd);
 
 	/**
 	 * If secure channel is established, we need to make sure that
@@ -1034,10 +1033,6 @@ static void osdp_pd_update(struct osdp_pd *pd)
 	}
 
 	osdp_phy_state_reset(pd, false);
-	if (ctx->command_complete_callback) {
-		ctx->command_complete_callback(ctx->command_complete_callback_arg,
-					       pd->cmd_id);
-	}
 }
 
 static void osdp_pd_set_attributes(struct osdp_pd *pd,
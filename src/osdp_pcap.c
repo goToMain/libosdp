84f27bf2dc69573ed855f86505dc419fbdda42d8
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2024-02-16 23:01:02 +0100

    libosdp: Add timestamp to trace files for uniqueness
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- src/osdp_pcap.c
+++ src/osdp_pcap.c
@@ -11,27 +11,31 @@
 void osdp_packet_capture_init(struct osdp_pd *pd)
 {
 	pcap_t *cap;
+	int n;
 	char path[128];
 
-	snprintf(path, 128, "/tmp/%spd-%d.pcap",
-		 is_pd_mode(pd) ? "" : "cp-", pd->address);
+	n = snprintf(path, sizeof(path), "osdp-trace-%spd-%d-",
+		     is_pd_mode(pd) ? "" : "cp-", pd->address);
+	n += add_iso8601_utc_datetime(path + n, sizeof(path) - n);
+	strcpy(path + n, ".pcap");
 	cap = pcap_create(path, OSDP_PACKET_BUF_SIZE,
 			  OSDP_PCAP_LINK_TYPE);
-	pd->packet_capture_ctx = (void *)cap;
 	if (cap) {
-		LOG_WRN("Capturing packets to '%s';"
-			" a graceful libosdp teardown is required", path);
+		LOG_WRN("Tracing: capturing packets to '%s'");
+		LOG_WRN("Tracing: a graceful teardown of libosdp ctx is required"
+			" for a complete trace file to be produced.", path);
 	} else {
-		LOG_WRN("Packet capture init failed;"
-			" check if '%s' is accessible", path);
+		LOG_WRN("Tracing: packet capture init failed;"
+			" check if path '%s' is accessible", path);
 	}
+	pd->packet_capture_ctx = (void *)cap;
 }
 
 void osdp_packet_capture_finish(struct osdp_pd *pd)
 {
 	pcap_t *cap = pd->packet_capture_ctx;
 
-	assert(ctx);
+	assert(cap);
 	pcap_dump(cap);
 }
 
@@ -39,7 +43,7 @@ void osdp_capture_packet(struct osdp_pd *pd, uint8_t *buf, int len)
 {
 	pcap_t *cap = pd->packet_capture_ctx;
 
-	assert(ctx);
+	assert(cap);
 	assert(len <= OSDP_PACKET_BUF_SIZE);
 	pcap_add_record(cap, buf, len);
 }

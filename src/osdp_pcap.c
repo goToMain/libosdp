924958b9dbd073977245df8287455f8407edaadc
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2024-02-17 18:46:13 +0100

    libosdp: pcap: Log the number of packets captured
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- src/osdp_pcap.c
+++ src/osdp_pcap.c
@@ -20,12 +20,12 @@ void osdp_packet_capture_init(struct osdp_pd *pd)
 	strcpy(path + n, ".pcap");
 	cap = pcap_start(path, OSDP_PACKET_BUF_SIZE, OSDP_PCAP_LINK_TYPE);
 	if (cap) {
-		LOG_WRN("Tracing: capturing packets to '%s'");
-		LOG_WRN("Tracing: a graceful teardown of libosdp ctx is required"
-			" for a complete trace file to be produced.", path);
+		LOG_WRN("Capturing packets to '%s'", path);
+		LOG_WRN("A graceful teardown of libosdp ctx is required"
+			" for a complete trace file to be produced.");
 	} else {
-		LOG_WRN("Tracing: packet capture init failed;"
-			" check if path '%s' is accessible", path);
+		LOG_ERR("Packet capture init failed; check if path '%s'"
+			" is accessible", path);
 	}
 	pd->packet_capture_ctx = (void *)cap;
 }
@@ -33,9 +33,15 @@ void osdp_packet_capture_init(struct osdp_pd *pd)
 void osdp_packet_capture_finish(struct osdp_pd *pd)
 {
 	pcap_t *cap = pd->packet_capture_ctx;
+	size_t num_packets;
 
 	assert(cap);
-	pcap_stop(cap);
+	num_packets = cap->num_packets;
+	if (pcap_stop(cap)) {
+		LOG_ERR("Unable to stop capture (flush/close failed)");
+		return;
+	}
+	LOG_INF("Captured %d packets", num_packets);
 }
 
 void osdp_capture_packet(struct osdp_pd *pd, uint8_t *buf, int len)

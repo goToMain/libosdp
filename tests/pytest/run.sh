e68bc9cfb55cb3b319c5166856e8bc3818de48b6
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2024-02-29 23:34:26 +0100

    pytest: Add some stage info for run.sh
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- tests/pytest/run.sh
+++ tests/pytest/run.sh
@@ -3,14 +3,19 @@
 PYTEST_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd ${PYTEST_DIR}
 
+echo "[-] Creating an isolated environment.."
 rm -rf __pycache__/
-rm -rf .venv ../../python/{build,dist,libosdp.egg-info}
+rm -rf .venv ../../python/{build,dist,libosdp.egg-info,vendor}
 python3 -m venv .venv
 source ./.venv/bin/activate
 
+echo "[-] Installing dependencies.."
 pip install pytest
+
+echo "[-] Installing libosdp.."
 pushd ../../python
 python3 setup.py install
 popd
 
+echo "[-] Running tests capturing all output.."
 pytest -vv --show-capture=all

bd15c2b71fb2edb9cfe51251e1795592d5d95f82
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2024-03-28 16:24:31 +0100

    phy: Enhance package scan logging by printing mark-byte
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- src/osdp_phy.c
+++ src/osdp_phy.c
@@ -518,8 +518,9 @@ int osdp_phy_check_packet(struct osdp_pd *pd)
 		}
 		pd->packet_len = ret;
 		if (pd->packet_scan_skip) {
-			LOG_WRN("Packet scan skipped %u bytes before SoM",
-				pd->packet_scan_skip);
+			LOG_WRN("Packet scan skipped:%u mark:%d",
+				pd->packet_scan_skip,
+				ISSET_FLAG(pd, PD_FLAG_PKT_HAS_MARK));
 			pd->packet_scan_skip = 0;
 		}
 	}

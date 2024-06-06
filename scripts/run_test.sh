120ad815cffafabba4a4b0c518658c5709c00a9a
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2024-06-06 22:33:41 +0200

    scripts: Add a tests runner scripts
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

---
+++ scripts/run_test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+TEST_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIR="${TEST_DIR}/../"
+
+echo "[-] Running make check"
+pushd ${ROOT_DIR}
+rm -rf build
+./configure.sh -f
+make check
+popd
+
+echo "[-] Running cmake unit-tests"
+rm -rf build && mkdir build && pushd build
+cmake ${ROOT_DIR}
+make check-ut
+popd
+
+echo "[-] Creating an isolated environment.."
+pushd ${TEST_DIR}
+rm -rf __pycache__/
+rm -rf .venv ${ROOT_DIR}/python/{build,dist,libosdp.egg-info,vendor}
+python3 -m venv .venv
+source ./.venv/bin/activate
+pip install --upgrade pip
+
+echo "[-] Installing dependencies.."
+pushd "${ROOT_DIR}/tests/pytest"
+pip install -r requirements.txt
+
+echo "[-] Installing libosdp.."
+pip install "${ROOT_DIR}/python"
+
+echo "[-] Running tests capturing all output.."
+pytest -vv --show-capture=all
+popd

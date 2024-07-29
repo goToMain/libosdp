goToMain/libosdp

commit 2e07d66da7fd6b4a3dcccde6cd4ed64a617f4d93

libosdp: Switch to pcap based packet tracing
Instead of using our own way of dumping packets and then pulling hairs
out to figure out which byte means what every couple of months, let's
actually walk the last mile and produce proper packet capture files that
can then be viewed in external tools such as wireshark.

Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_cp.c
@@ -1359,6 +1359,10 @@ static struct osdp *__cp_setup(int num_pd, const osdp_pd_info_t *info_list)
 		logger_get_default(&pd->logger);
 		snprintf(name, sizeof(name), "OSDP: CP: PD-%d", pd->address);
 		logger_set_name(&pd->logger, name);
+
+		if (IS_ENABLED(CONFIG_OSDP_PACKET_TRACE)) {
+			osdp_packet_capture_init(pd);
+		}
 	}
 
 	if (cp_detect_connection_topology(ctx)) {
@@ -1394,6 +1398,15 @@ OSDP_EXPORT
 void osdp_cp_teardown(osdp_t *ctx)
 {
 	input_check(ctx);
+	int i;
+	struct osdp_pd *pd;
+
+	if (IS_ENABLED(CONFIG_OSDP_PACKET_TRACE)) {
+		for (i = 0; i < NUM_PD(ctx); i++) {
+			pd = osdp_to_pd(ctx, 0);
+			osdp_packet_capture_finish(pd);
+		}
+	}
 
 	safe_free(osdp_to_pd(ctx, 0));
 	safe_free(TO_OSDP(ctx)->channel_lock);

configure.sh
@@ -142,6 +142,10 @@ LIBOSDP_SOURCES+=" src/osdp_common.c src/osdp_phy.c src/osdp_sc.c src/osdp_file.
 LIBOSDP_SOURCES+=" utils/src/list.c utils/src/queue.c utils/src/slab.c utils/src/utils.c"
 LIBOSDP_SOURCES+=" utils/src/disjoint_set.c utils/src/logger.c"
 
+if [[ ! -z "${PACKET_TRACE}" ]]; then
+	LIBOSDP_SOURCES+=" src/osdp_pcap.c utils/src/pcap_gen.c"
+fi
+
 if [[ -z "${STATIC_PD}" ]]; then
 	LIBOSDP_SOURCES+=" src/osdp_cp.c"
 	TARGETS="cp_app pd_app"

python/setup.py
@@ -150,6 +150,11 @@ osdp_sys_include = [
 
 other_files = [
     "src/osdp_config.h.in",
+
+    # Optional when PACKET_TRACE is enabled
+    "src/osdp_pcap.c",
+    "utils/include/utils/pcap_gen.h",
+    "utils/src/pcap_gen.c",
 ]
 
 source_files = utils_sources + lib_sources + osdp_sys_sources
@@ -160,6 +165,18 @@ try_vendor_sources(
     "vendor"
 )
 
+definitions = [
+    # "CONFIG_OSDP_PACKET_TRACE",
+    # "CONFIG_OSDP_DATA_TRACE",
+    # "CONFIG_OSDP_SKIP_MARK_BYTE",
+]
+
+if "CONFIG_OSDP_PACKET_TRACE" in definitions:
+    source_files += [
+        "src/osdp_pcap.c",
+        "utils/src/pcap_gen.c",
+    ]
+
 source_files = add_prefix_to_path(source_files, "vendor")
 
 include_dirs = [
@@ -168,12 +185,6 @@ include_dirs = [
     "vendor/src",
     "vendor/python/osdp_sys",
     "vendor/src/crypto"
-]
-
-definitions = [
-    # "CONFIG_OSDP_PACKET_TRACE",
-    # "CONFIG_OSDP_DATA_TRACE",
-    # "CONFIG_OSDP_SKIP_MARK_BYTE",
 ]
 
 compile_args = (

src/CMakeLists.txt
@@ -62,6 +62,12 @@ if (NOT CONFIG_OSDP_STATIC_PD)
 	)
 endif()
 
+if (CONFIG_OSDP_PACKET_TRACE)
+	list(APPEND LIB_OSDP_UTILS_SRC
+		${CMAKE_CURRENT_SOURCE_DIR}/osdp_pcap.c
+	)
+endif()
+
 list(APPEND LIB_OSDP_PRIVATE_INCLUDE_DIRS
 	${CMAKE_CURRENT_SOURCE_DIR}
 	${CMAKE_CURRENT_BINARY_DIR}
@@ -128,6 +134,13 @@ list(APPEND LIB_OSDP_UTILS_SRC
 	${PROJECT_SOURCE_DIR}/utils/src/disjoint_set.c
 	${PROJECT_SOURCE_DIR}/utils/src/logger.c
 )
+
+if (CONFIG_OSDP_PACKET_TRACE)
+	list(APPEND LIB_OSDP_UTILS_SRC
+		${PROJECT_SOURCE_DIR}/utils/src/pcap_gen.c
+	)
+endif()
+
 set(LIB_OSDP_UTILS_SRC ${LIB_OSDP_UTILS_SRC} PARENT_SCOPE)
 
 # add source files of utils instead of linking it. See comment above.

src/osdp_common.h
@@ -358,7 +358,11 @@ struct osdp_pd {
 	void *command_callback_arg;
 	pd_command_callback_t command_callback;
 
-	logger_t logger;       /* logger context (from utils/logger.h) */
+	/* logger context (from utils/logger.h) */
+	logger_t logger;
+
+	/* Opaque packet capture pointer (see osdp_pcap.c) */
+	void *packet_capture_ctx;
 };
 
 struct osdp {
@@ -378,6 +382,17 @@ struct osdp {
 static inline void cp_keyset_complete(struct osdp_pd *pd, bool restart_sc) { }
 #else
 void cp_keyset_complete(struct osdp_pd *pd, bool restart_sc);
+#endif
+
+#ifdef CONFIG_OSDP_PACKET_TRACE
+void osdp_packet_capture_init(struct osdp_pd *pd);
+void osdp_packet_capture_finish(struct osdp_pd *pd);
+void osdp_capture_packet(struct osdp_pd *pd, uint8_t *buf, int len);
+#else
+static inline void osdp_packet_capture_init(struct osdp_pd *pd) { }
+static inline void osdp_packet_capture_finish(struct osdp_pd *pd) { }
+static inline void osdp_capture_packet(struct osdp_pd *pd,
+				       uint8_t *buf, int len) { }
 #endif
 
 void osdp_keyset_complete(struct osdp_pd *pd);

src/osdp_config.h.in
@@ -35,5 +35,6 @@
 #define OSDP_FILE_ERROR_RETRY_MAX               (10)
 #define OSDP_PD_MAX                             (126)
 #define OSDP_CMD_ID_OFFSET                      (5)
+#define OSDP_PCAP_LINK_TYPE                     (162)
 
 #endif /* _OSDP_CONFIG_H_ */

src/osdp_pcap.c
@@ -1,0 +1,45 @@
+/*
+ * Copyright (c) 2024 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <utils/pcap_gen.h>
+
+#include "osdp_common.h"
+
+void osdp_packet_capture_init(struct osdp_pd *pd)
+{
+	pcap_t *cap;
+	char path[128];
+
+	snprintf(path, 128, "/tmp/%spd-%d.pcap",
+		 is_pd_mode(pd) ? "" : "cp-", pd->address);
+	cap = pcap_create(path, OSDP_PACKET_BUF_SIZE,
+			  OSDP_PCAP_LINK_TYPE);
+	pd->packet_capture_ctx = (void *)cap;
+	if (cap) {
+		LOG_WRN("Capturing packets to '%s';"
+			" a graceful libosdp teardown is required", path);
+	} else {
+		LOG_WRN("Packet capture init failed;"
+			" check if '%s' is accessible", path);
+	}
+}
+
+void osdp_packet_capture_finish(struct osdp_pd *pd)
+{
+	pcap_t *cap = pd->packet_capture_ctx;
+
+	assert(ctx);
+	pcap_dump(cap);
+}
+
+void osdp_capture_packet(struct osdp_pd *pd, uint8_t *buf, int len)
+{
+	pcap_t *cap = pd->packet_capture_ctx;
+
+	assert(ctx);
+	assert(len <= OSDP_PACKET_BUF_SIZE);
+	pcap_add_record(cap, buf, len);
+}

src/osdp_pd.c
@@ -1162,6 +1162,10 @@ osdp_t *osdp_pd_setup(const osdp_pd_info_t *info)
 
 	SET_FLAG(pd, PD_FLAG_PD_MODE); /* used in checks in phy */
 
+	if (IS_ENABLED(CONFIG_OSDP_PACKET_TRACE)) {
+		osdp_packet_capture_init(pd);
+	}
+
 	LOG_PRINT("Setup complete; LibOSDP-%s %s",
 		  osdp_get_version(), osdp_get_source_info());
 
@@ -1175,9 +1179,14 @@ OSDP_EXPORT
 void osdp_pd_teardown(osdp_t *ctx)
 {
 	assert(ctx);
+	struct osdp_pd *pd = osdp_to_pd(ctx, 0);
+
+	if (IS_ENABLED(CONFIG_OSDP_PACKET_TRACE)) {
+		osdp_packet_capture_finish(pd);
+	}
 
 #ifndef CONFIG_OSDP_STATIC_PD
-	safe_free(osdp_to_pd(ctx, 0));
+	safe_free(pd);
 	safe_free(ctx);
 #endif
 }

commit cf4e4b97516a730e453f900a652d067d14909074

cp: Fix segfault in osdp_cp_teardown()
osdp_cp_teardown() calls osdp_to_pd(ctx, 0) for all loop entries which
causes a segfault.

This issue was repported and fixed-by Marek Felskowski over email.

Fixes: 2e07d66da7fd6b4a3dcccde6cd4ed64a617f4d93
Authored-by: Marek Felskowski <mfelskowski2@roger.pl>
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_cp.c
@@ -1464,7 +1464,7 @@ void osdp_cp_teardown(osdp_t *ctx)
 	if (IS_ENABLED(CONFIG_OSDP_PACKET_TRACE) ||
 	    IS_ENABLED(CONFIG_OSDP_DATA_TRACE)) {
 		for (i = 0; i < NUM_PD(ctx); i++) {
-			pd = osdp_to_pd(ctx, 0);
+			pd = osdp_to_pd(ctx, i);
 			osdp_packet_capture_finish(pd);
 		}
 	}
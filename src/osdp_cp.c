goToMain/libosdp

commit caadc3eff418cd99e4da9781fa3aa9b146786661

libosdp: Rework status command and event handling
It was reported in #154 that LibOSDP has no way for PD app to answer
some commands. To fix this, a bit of rework was needed in how we look at
the status event and command.

Note: This commit breaks API and ABI since we have changed command and
event numbers (amd removed existing ones). Since we are in the works for
a major relase from master, and we have a stable 2.4.x branch, this
should be okay.

Fixes: #154
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_cp.c
@@ -501,9 +501,10 @@ static int cp_decode_response(struct osdp_pd *pd, uint8_t *buf, int len)
 		for (i = 0; i < len; i++) {
 			status_mask |= !!buf[pos++] << i;
 		}
-		event.type = OSDP_EVENT_IO;
-		event.io.type = 1; // output
-		event.io.status = status_mask;
+		event.type = OSDP_EVENT_STATUS;
+		event.status.type = OSDP_EVENT_STATUS_TYPE_OUTPUT;
+		event.status.nr_entries = len;
+		event.status.mask = status_mask;
 		memcpy(pd->ephemeral_data, &event, sizeof(event));
 		make_request(pd, CP_REQ_EVENT_SEND);
 		ret = OSDP_CP_ERR_NONE;
@@ -520,9 +521,10 @@ static int cp_decode_response(struct osdp_pd *pd, uint8_t *buf, int len)
 		for (i = 0; i < len; i++) {
 			status_mask |= !!buf[pos++] << i;
 		}
-		event.type = OSDP_EVENT_IO;
-		event.io.type = 0; // input
-		event.io.status = status_mask;
+		event.type = OSDP_EVENT_STATUS;
+		event.status.type = OSDP_EVENT_STATUS_TYPE_INPUT;
+		event.status.nr_entries = len;
+		event.status.mask = status_mask;
 		memcpy(pd->ephemeral_data, &event, sizeof(event));
 		make_request(pd, CP_REQ_EVENT_SEND);
 		ret = OSDP_CP_ERR_NONE;
@@ -533,8 +535,10 @@ static int cp_decode_response(struct osdp_pd *pd, uint8_t *buf, int len)
 			break;
 		}
 		event.type = OSDP_EVENT_STATUS;
-		event.status.tamper = buf[pos++];
-		event.status.power = buf[pos++];
+		event.status.type = OSDP_EVENT_STATUS_TYPE_LOCAL;
+		event.status.nr_entries = 2;
+		event.status.mask = !!buf[pos++];
+		event.status.mask = !!buf[pos++] << 1;
 		memcpy(pd->ephemeral_data, &event, sizeof(event));
 		make_request(pd, CP_REQ_EVENT_SEND);
 		ret = OSDP_CP_ERR_NONE;
@@ -543,11 +547,12 @@ static int cp_decode_response(struct osdp_pd *pd, uint8_t *buf, int len)
 		if (len != REPLY_RSTATR_DATA_LEN) {
 			break;
 		}
-		if (buf[pos++]) {
-			SET_FLAG(pd, PD_FLAG_R_TAMPER);
-		} else {
-			CLEAR_FLAG(pd, PD_FLAG_R_TAMPER);
-		}
+		event.type = OSDP_EVENT_STATUS;
+		event.status.type = OSDP_EVENT_STATUS_TYPE_REMOTE;
+		event.status.nr_entries = 1;
+		event.status.mask = !!buf[pos++];
+		memcpy(pd->ephemeral_data, &event, sizeof(event));
+		make_request(pd, CP_REQ_EVENT_SEND);
 		ret = OSDP_CP_ERR_NONE;
 		break;
 	case REPLY_COM:
@@ -785,9 +790,10 @@ int cp_translate_cmd(struct osdp_pd *pd, struct osdp_cmd *cmd)
 	case OSDP_CMD_MFG:    return CMD_MFG;
 	case OSDP_CMD_STATUS:
 		switch (cmd->status.type) {
-		case OSDP_CMD_STATUS_QUERY_LOCAL:  return CMD_LSTAT;
 		case OSDP_CMD_STATUS_QUERY_INPUT:  return CMD_ISTAT;
 		case OSDP_CMD_STATUS_QUERY_OUTPUT: return CMD_OSTAT;
+		case OSDP_CMD_STATUS_QUERY_LOCAL:  return CMD_LSTAT;
+		case OSDP_CMD_STATUS_QUERY_REMOTE: return CMD_RSTAT;
 		default: return -1;
 		}
 	case OSDP_CMD_KEYSET:

include/osdp.h
@@ -197,7 +197,7 @@ enum osdp_pd_nak_code_e {
 	 */
 	OSDP_PD_NAK_RECORD,
 	/**
-	 * @brief Dummy
+	 * @brief NAK codes max value
 	 */
 	OSDP_PD_NAK_SENTINEL
 };
@@ -527,6 +527,14 @@ enum osdp_command_status_query_e {
 	 * @brief Query status of the ouputs attached the PD
 	 */
 	OSDP_CMD_STATUS_QUERY_OUTPUT,
+	/**
+	 * @brief Query status of the remote readers attached to the PD
+	 */
+	OSDP_CMD_STATUS_QUERY_REMOTE,
+	/**
+	 * @brief Status query max value
+	 */
+	OSDP_CMD_STATUS_QUERY_SENTINEL
 };
 
 /**
@@ -534,6 +542,8 @@ enum osdp_command_status_query_e {
  */
 struct osdp_cmd_status {
 	enum osdp_command_status_query_e type;
+	int nr_entries;
+	uint32_t mask;
 };
 
 /**
@@ -659,45 +669,72 @@ struct osdp_event_mfgrep {
 };
 
 /**
- * @brief OSDP Event input/output status
- *
- * This event is used by the PD to indicate input/output status changes. Upto a
- * maximum of 32 input/output status can be reported. The values of the least
- * significant N bit of status are considered, where N is the number of items as
- * described in the corresponding capability codes: OSDP_PD_CAP_OUTPUT_CONTROL
- * and OSDP_PD_CAP_CONTACT_STATUS_MONITORING.
- *
- * @param type 0 - input; 1 - output
- * @param status bit mask of current input status.
+ * @brief Status event types
  */
-struct osdp_event_io {
-	int type;
-	uint32_t status;
+enum osdp_event_status_type_e {
+	/**
+	 * @brief Input status event
+	 */
+	OSDP_EVENT_STATUS_TYPE_INPUT,
+	/**
+	 * @brief Output status event
+	 */
+	OSDP_EVENT_STATUS_TYPE_OUTPUT,
+	/**
+	 * @brief Remote tamper and power status event
+	 *
+	 * Bit-0: tamper
+	 * Bit-1: power
+	 */
+	OSDP_EVENT_STATUS_TYPE_REMOTE,
+	/**
+	 * @brief Local tamper and power status event
+	 *
+	 * Bit-0: tamper
+	 * Bit-1: power
+	 */
+	OSDP_EVENT_STATUS_TYPE_LOCAL,
 };
 
 /**
- * @brief OSDP Event tamper and power status
+ * @brief OSDP Event input/output status
  *
- * The event indicates the local tamper and power status of the PD. When either
- * of these statuses change, PD notifies the CP as a response to POLL command.
+ * This event is used by the PD to indicate various status change reports. Upto
+ * a maximum of 32 statuses can be reported using this API.
  *
- * @param tamper tamper status: 0 - normal; 1 - tamper
- * @param power power status: 0 - normal; 1 - power failure
+ * @param type The kind of event to report see `enum osdp_event_status_type_e`
+ * @param nr_entries Number of valid bits in `status`
+ * @param status Status bit mask
  */
 struct osdp_event_status {
-	uint8_t tamper;
-	uint8_t power;
+	enum osdp_event_status_type_e type;
+	int nr_entries;
+	uint32_t mask;
 };
 
 /**
  * @brief OSDP PD Events
  */
 enum osdp_event_type {
-	OSDP_EVENT_CARDREAD,
+	/**
+	 * @brief Card read event
+	 */
+	OSDP_EVENT_CARDREAD = 1,
+	/**
+	 * @brief Key press event
+	 */
 	OSDP_EVENT_KEYPRESS,
+	/**
+	 * @brief Manufacturer specific reply event
+	 */
 	OSDP_EVENT_MFGREP,
-	OSDP_EVENT_IO,
+	/**
+	 * @brief Status event
+	 */
 	OSDP_EVENT_STATUS,
+	/**
+	 * @brief Event maximum
+	 */
 	OSDP_EVENT_SENTINEL
 };
 
@@ -715,7 +752,6 @@ struct osdp_event {
 		struct osdp_event_keypress keypress;
 		struct osdp_event_cardread cardread;
 		struct osdp_event_mfgrep mfgrep;
-		struct osdp_event_io io;
 		struct osdp_event_status status;
 	};
 };

python/osdp_sys/pd.c
@@ -88,22 +88,12 @@ static int pd_command_cb(void *arg, struct osdp_cmd *cmd)
 
 	arglist = Py_BuildValue("(O)", dict);
 	result = PyObject_CallObject(self->command_cb, arglist);
+	PyArg_ParseTuple(result, "IO", &ret_val, &result);
 
-	if (result && PyDict_Check(result)) {
-		if (pyosdp_dict_get_int(result, "return_code", &ret_val) == 0) {
-			/**
-			 * If ret_val > 0 and we can make a MFGREP command
-			 * out of the dict from python, we will reply with it.
-			 * If not, reply with NAK.
-			 */
-			if (ret_val > 0) {
-				memset(cmd, 0, sizeof(struct osdp_cmd));
-				if (pyosdp_make_struct_cmd(cmd, result) < 0)
-					ret_val = -1;
-				else if (cmd->id != OSDP_CMD_MFG)
-					ret_val = -1;
-			}
-		}
+	if (ret_val == 0 && result && PyDict_Check(result)) {
+		memset(cmd, 0, sizeof(struct osdp_cmd));
+		if (pyosdp_make_struct_cmd(cmd, result) < 0)
+			ret_val = -1;
 	}
 
 	Py_XDECREF(dict);

src/osdp_pd.c
@@ -151,28 +151,24 @@ static int pd_translate_event(struct osdp_pd *pd, struct osdp_event *event)
 	case OSDP_EVENT_KEYPRESS:
 		reply_code = REPLY_KEYPPAD;
 		break;
-	case OSDP_EVENT_IO:
-		if (event->io.type) {
-			reply_code = REPLY_OSTATR;
-		} else {
+	case OSDP_EVENT_STATUS:
+		switch(event->status.type) {
+		case OSDP_EVENT_STATUS_TYPE_INPUT:
 			reply_code = REPLY_ISTATR;
+			break;
+		case OSDP_EVENT_STATUS_TYPE_OUTPUT:
+			reply_code = REPLY_OSTATR;
+			break;
+		case OSDP_EVENT_STATUS_TYPE_LOCAL:
+			reply_code = REPLY_LSTATR;
+			break;
+		case OSDP_EVENT_STATUS_TYPE_REMOTE:
+			reply_code = REPLY_RSTATR;
+			break;
 		}
 		break;
 	case OSDP_EVENT_MFGREP:
 		reply_code = REPLY_MFGREP;
-		break;
-	case OSDP_EVENT_STATUS:
-		if (event->status.tamper == 1) {
-			SET_FLAG(pd, PD_FLAG_TAMPER);
-		} else {
-			CLEAR_FLAG(pd, PD_FLAG_TAMPER);
-		}
-		if (event->status.power == 1) {
-			SET_FLAG(pd, PD_FLAG_POWER);
-		} else {
-			CLEAR_FLAG(pd, PD_FLAG_POWER);
-		}
-		reply_code = REPLY_LSTATR;
 		break;
 	default:
 		LOG_ERR("Unknown event type %d", event->type);
@@ -313,35 +309,72 @@ static int pd_decode_command(struct osdp_pd *pd, uint8_t *buf, int len)
 		if (len != CMD_LSTAT_DATA_LEN) {
 			break;
 		}
+		cmd.id = OSDP_CMD_STATUS;
+		cmd.status.type = OSDP_CMD_STATUS_QUERY_LOCAL;
+		if (do_command_callback(pd, &cmd)) {
+			break;
+		}
+		event = (struct osdp_event *)pd->ephemeral_data;
+		event->status.type = OSDP_EVENT_STATUS_TYPE_LOCAL;
+		event->status.nr_entries = cmd.status.nr_entries;
+		event->status.mask = cmd.status.mask;
 		pd->reply_id = REPLY_LSTATR;
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	case CMD_ISTAT:
-		if (len != CMD_ISTAT_DATA_LEN) {
+		if (len != CMD_ISTAT_DATA_LEN || !pd->command_callback) {
 			break;
 		}
 		if (!pd_cmd_cap_ok(pd, NULL)) {
 			ret = OSDP_PD_ERR_REPLY;
 			break;
 		}
+		cmd.id = OSDP_CMD_STATUS;
+		cmd.status.type = OSDP_CMD_STATUS_QUERY_INPUT;
+		if (do_command_callback(pd, &cmd)) {
+			break;
+		}
+		event = (struct osdp_event *)pd->ephemeral_data;
+		event->type = OSDP_EVENT_STATUS;
+		event->status.type = OSDP_EVENT_STATUS_TYPE_INPUT;
+		event->status.nr_entries = cmd.status.nr_entries;
+		event->status.mask = cmd.status.mask;
 		pd->reply_id = REPLY_ISTATR;
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	case CMD_OSTAT:
-		if (len != CMD_OSTAT_DATA_LEN) {
+		if (len != CMD_OSTAT_DATA_LEN || !pd->command_callback) {
 			break;
 		}
 		if (!pd_cmd_cap_ok(pd, NULL)) {
 			ret = OSDP_PD_ERR_REPLY;
 			break;
 		}
+		cmd.id = OSDP_CMD_STATUS;
+		cmd.status.type = OSDP_CMD_STATUS_QUERY_OUTPUT;
+		if (do_command_callback(pd, &cmd)) {
+			break;
+		}
+		event = (struct osdp_event *)pd->ephemeral_data;
+		event->status.type = OSDP_EVENT_STATUS_TYPE_OUTPUT;
+		event->status.nr_entries = cmd.status.nr_entries;
+		event->status.mask = cmd.status.mask;
 		pd->reply_id = REPLY_OSTATR;
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	case CMD_RSTAT:
-		if (len != CMD_RSTAT_DATA_LEN) {
+		if (len != CMD_RSTAT_DATA_LEN || !pd->command_callback) {
 			break;
 		}
+		cmd.id = OSDP_CMD_STATUS;
+		cmd.status.type = OSDP_CMD_STATUS_QUERY_REMOTE;
+		if (do_command_callback(pd, &cmd)) {
+			break;
+		}
+		event = (struct osdp_event *)pd->ephemeral_data;
+		event->status.type = OSDP_EVENT_STATUS_TYPE_REMOTE;
+		event->status.nr_entries = cmd.status.nr_entries;
+		event->status.mask = cmd.status.mask;
 		pd->reply_id = REPLY_RSTATR;
 		ret = OSDP_PD_ERR_NONE;
 		break;
@@ -721,40 +754,46 @@ static int pd_build_reply(struct osdp_pd *pd, uint8_t *buf, int max_len)
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	case REPLY_OSTATR: {
-		int n = pd->cap[OSDP_PD_CAP_OUTPUT_CONTROL].num_items;
-
-		assert_buf_len(n + 1, max_len);
 		event = (struct osdp_event *)pd->ephemeral_data;
+		int n = pd->cap[OSDP_PD_CAP_OUTPUT_CONTROL].num_items;
+		if (event->status.nr_entries != n) {
+			break;
+		}
+		assert_buf_len(n + 1, max_len);
 		buf[len++] = pd->reply_id;
 		for (i = 0; i < n; i++) {
-			buf[len++] = !!(event->io.status & (1 << i));
+			buf[len++] = !!(event->status.mask & (1 << i));
 		}
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	}
 	case REPLY_ISTATR: {
-		int n = pd->cap[OSDP_PD_CAP_CONTACT_STATUS_MONITORING].num_items;
-
-		assert_buf_len(n + 1, max_len);
 		event = (struct osdp_event *)pd->ephemeral_data;
+		int n = pd->cap[OSDP_PD_CAP_CONTACT_STATUS_MONITORING].num_items;
+		if (event->status.nr_entries != n) {
+			break;
+		}
+		assert_buf_len(n + 1, max_len);
 		buf[len++] = pd->reply_id;
 		for (i = 0; i < n; i++) {
-			buf[len++] = !!(event->io.status & (1 << i));
+			buf[len++] = !!(event->status.mask & (1 << i));
 		}
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	}
 	case REPLY_LSTATR:
 		assert_buf_len(REPLY_LSTATR_LEN, max_len);
+		event = (struct osdp_event *)pd->ephemeral_data;
 		buf[len++] = pd->reply_id;
-		buf[len++] = ISSET_FLAG(pd, PD_FLAG_TAMPER);
-		buf[len++] = ISSET_FLAG(pd, PD_FLAG_POWER);
+		buf[len++] = BIT_IS_SET(event->status.mask, 0); // tamper
+		buf[len++] = BIT_IS_SET(event->status.mask, 1); // power
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	case REPLY_RSTATR:
 		assert_buf_len(REPLY_RSTATR_LEN, max_len);
+		event = (struct osdp_event *)pd->ephemeral_data;
 		buf[len++] = pd->reply_id;
-		buf[len++] = ISSET_FLAG(pd, PD_FLAG_R_TAMPER);
+		buf[len++] = BIT_IS_SET(event->status.mask, 0); // power
 		ret = OSDP_PD_ERR_NONE;
 		break;
 	case REPLY_KEYPPAD:

commit c6ef1bee8ef14273717f415e6619b57abc8aa9b0

cp: Fix loss of tamper bit when saving power bit in status event
Fixes: caadc3eff418cd99e4da9781fa3aa9b146786661
Signed-off-by: Dwayne Forsyth <dwaynef@2dkits.com>
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_cp.c
@@ -527,7 +527,7 @@ static int cp_decode_response(struct osdp_pd *pd, uint8_t *buf, int len)
 		event.status.type = OSDP_STATUS_REPORT_LOCAL;
 		event.status.nr_entries = 2;
 		event.status.mask = !!buf[pos++];
-		event.status.mask = !!buf[pos++] << 1;
+		event.status.mask |= !!buf[pos++] << 1;
 		memcpy(pd->ephemeral_data, &event, sizeof(event));
 		make_request(pd, CP_REQ_EVENT_SEND);
 		ret = OSDP_CP_ERR_NONE;
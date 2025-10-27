goToMain/libosdp

commit 969dbf8f9a0f259b4ea48a99467aa35061da1b73

PD: Add internal command validation
According OSDP specification 2.2.2:

 - osdp_LED : The ON Time OFF Time values cannot both be set to zero.
 - osdp_BUZ : The ON duration of the sound, in units of 100 ms. Must be nonzero unless the tone code is 0x01 (off.)

This patch adds a validation method which acts as a gate to the
application callback, thereby sending a NAK(9) to the CP if the
validation fails.

Related-to: #246
Co-Developed-By: Samuel Gouraud <sgouraud@fdi-access.com>
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_pd.c
@@ -195,11 +195,44 @@ static int pd_translate_event(struct osdp_pd *pd, struct osdp_event *event)
 	return reply_code;
 }
 
+static bool validate_command(struct osdp_pd *pd, struct osdp_cmd *cmd)
+{
+	bool result = true;
+
+	switch (cmd->id) {
+	case OSDP_CMD_LED:
+		/* The ON Time OFF Time values cannot both be set to zero */
+		if (cmd->led.temporary.control_code == 0x02 && /* SET */
+		    cmd->led.temporary.on_count == 0 &&
+		    cmd->led.temporary.off_count == 0) {
+			result = false;
+		}
+		if (cmd->led.permanent.control_code == 0x01 &&  /* SET */
+		    cmd->led.permanent.on_count == 0 &&
+		    cmd->led.permanent.off_count == 0) {
+			result = false;
+		}
+		break;
+	case OSDP_CMD_OUTPUT:
+		/* ON duration must nonzero unless the control_code is 0x01 */
+		if (cmd->buzzer.on_count == 0 &&
+		    cmd->buzzer.control_code != 1) {
+			result = false;
+		}
+		break;
+	}
+
+	if (!result) {
+		LOG_ERR("Command validation failed!");
+	}
+	return result;
+}
+
 static bool do_command_callback(struct osdp_pd *pd, struct osdp_cmd *cmd)
 {
 	int ret = -1;
 
-	if (pd->command_callback) {
+	if (validate_command(pd, cmd) && pd->command_callback) {
 		ret = pd->command_callback(pd->command_callback_arg, cmd);
 	}
 	if (ret != 0) {

commit 83236e00a8693aea77cbd81f4751ee4959c88f98

fixup! PD: Add internal command validation
Fix a typo in command validator.

Fixes: 969dbf8f9a0f259b4ea48a99467aa35061da1b73
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

src/osdp_pd.c
@@ -213,7 +213,7 @@ static bool validate_command(struct osdp_pd *pd, struct osdp_cmd *cmd)
 			result = false;
 		}
 		break;
-	case OSDP_CMD_OUTPUT:
+	case OSDP_CMD_BUZZER:
 		/* ON duration must nonzero unless the control_code is 0x01 */
 		if (cmd->buzzer.on_count == 0 &&
 		    cmd->buzzer.control_code != 1) {
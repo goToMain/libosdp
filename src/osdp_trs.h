4c911531ab37e9d1e51d039083fd2fbd25271cb6
Aaron Tulino (Aaronjamt) <git@aaronjamt.com>
2025-11-15 23:50:06 -0700

    Change all `CONFIG_OSDP_TRS` to `OPT_BUILD_OSDP_TRS`

--- src/osdp_trs.h
+++ src/osdp_trs.h
@@ -9,14 +9,14 @@
 
 #include "osdp_common.h"
 
-#ifdef CONFIG_OSDP_TRS
+#ifdef OPT_BUILD_OSDP_TRS
 
 int osdp_trs_cmd_build(struct osdp_pd *pd, uint8_t *buf, int max_len);
 int osdp_trs_reply_decode(struct osdp_pd *pd, uint8_t *buf, int len);
 int osdp_trs_reply_build(struct osdp_pd *pd, uint8_t *buf, int max_len);
 int osdp_trs_cmd_decode(struct osdp_pd *pd, uint8_t *buf, int len);
 
-#else /* CONFIG_OSDP_TRS */
+#else /* OPT_BUILD_OSDP_TRS */
 
 static inline int osdp_trs_cmd_build(struct osdp_pd *pd, uint8_t *buf, int max_len)
 {
@@ -47,6 +47,6 @@ static inline int osdp_trs_cmd_decode(struct osdp_pd *pd, uint8_t *buf, int len)
 	return -1;
 }
 
-#endif /* CONFIG_OSDP_TRS */
+#endif /* OPT_BUILD_OSDP_TRS */
 
 #endif /* _OSDP_TRS_H_ */

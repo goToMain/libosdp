a70bddcfb6912e648d5ecc68d0902704e6fde561
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2026-03-14 00:08:38 +0100

    PlatformIO: Replace C99-style designated init
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- examples/platformio/cp.ino
+++ examples/platformio/cp.ino
@@ -8,6 +8,9 @@
 #include <osdp.hpp>
 
 OSDP::ControlPanel cp;
+osdp_pd_info_t pd_info[] = {
+    {},
+};
 
 int serial1_send_func(void *data, uint8_t *buf, int len)
 {
@@ -36,24 +39,28 @@ int serial1_recv_func(void *data, uint8_t *buf, int len)
     return read;
 }
 
-osdp_pd_info_t pd_info[] = {
-    {
-        .name = "pd[101]",
-        .baud_rate = (int)115200,
-        .address = 101,
-        .flags = 0,
-        .id = {},
-        .cap = nullptr,
-        .channel = {
-            .data = nullptr,
-            .id = 0,
-            .recv = serial1_recv_func,
-            .send = serial1_send_func,
-            .flush = nullptr
-        },
-        .scbk = nullptr,
-    },
-};
+void init_cp_info()
+{
+    pd_info[0].name = "pd[101]";
+    pd_info[0].baud_rate = 115200;
+    pd_info[0].address = 101;
+    pd_info[0].flags = 0;
+    pd_info[0].id.version = 0;
+    pd_info[0].id.model = 0;
+    pd_info[0].id.vendor_code = 0;
+    pd_info[0].id.serial_number = 0;
+    pd_info[0].id.firmware_version = 0;
+    pd_info[0].cap = nullptr;
+    pd_info[0].channel.data = nullptr;
+    pd_info[0].channel.id = 0;
+    pd_info[0].channel.recv = serial1_recv_func;
+    pd_info[0].channel.recv_pkt = nullptr;
+    pd_info[0].channel.send = serial1_send_func;
+    pd_info[0].channel.flush = nullptr;
+    pd_info[0].channel.release_pkt = nullptr;
+    pd_info[0].channel.close = nullptr;
+    pd_info[0].scbk = nullptr;
+}
 
 int event_handler(void *data, int pd, struct osdp_event *event)
 {
@@ -70,6 +77,7 @@ void setup()
 
     cp.logger_init("osdp::cp", OSDP_LOG_DEBUG, NULL);
 
+    init_cp_info();
     cp.setup(1, pd_info);
     cp.set_event_callback(event_handler, nullptr);
 }
--- examples/platformio/pd.ino
+++ examples/platformio/pd.ino
@@ -8,6 +8,12 @@
 #include <osdp.hpp>
 
 OSDP::PeripheralDevice pd;
+osdp_pd_info_t info_pd = {};
+static const struct osdp_pd_cap pd_cap[] = {
+    { OSDP_PD_CAP_READER_LED_CONTROL, 1, 1 },
+    { OSDP_PD_CAP_READER_AUDIBLE_OUTPUT, 1, 1 },
+    { static_cast<uint8_t>(-1), 0, 0 } /* Sentinel */
+};
 
 int serial1_send_func(void *data, uint8_t *buf, int len)
 {
@@ -36,40 +42,28 @@ int serial1_recv_func(void *data, uint8_t *buf, int len)
     return read;
 }
 
-osdp_pd_info_t info_pd = {
-    .name = "pd[101]",
-    .baud_rate = 9600,
-    .address = 101,
-    .flags = 0,
-    .id = {
-        .version = 1,
-        .model = 153,
-        .vendor_code = 31337,
-        .serial_number = 0x01020304,
-        .firmware_version = 0x0A0B0C0D,
-    },
-    .cap = (struct osdp_pd_cap []) {
-        {
-            .function_code = OSDP_PD_CAP_READER_LED_CONTROL,
-            .compliance_level = 1,
-            .num_items = 1
-        },
-        {
-            .function_code = OSDP_PD_CAP_READER_AUDIBLE_OUTPUT,
-            .compliance_level = 1,
-            .num_items = 1
-        },
-        { static_cast<uint8_t>(-1), 0, 0 } /* Sentinel */
-    },
-    .channel = {
-        .data = nullptr,
-        .id = 0,
-        .recv = serial1_recv_func,
-        .send = serial1_send_func,
-        .flush = nullptr
-    },
-    .scbk = nullptr,
-};
+void init_pd_info()
+{
+    info_pd.name = "pd[101]";
+    info_pd.baud_rate = 9600;
+    info_pd.address = 101;
+    info_pd.flags = 0;
+    info_pd.id.version = 1;
+    info_pd.id.model = 153;
+    info_pd.id.vendor_code = 31337;
+    info_pd.id.serial_number = 0x01020304;
+    info_pd.id.firmware_version = 0x0A0B0C0D;
+    info_pd.cap = pd_cap;
+    info_pd.channel.data = nullptr;
+    info_pd.channel.id = 0;
+    info_pd.channel.recv = serial1_recv_func;
+    info_pd.channel.recv_pkt = nullptr;
+    info_pd.channel.send = serial1_send_func;
+    info_pd.channel.flush = nullptr;
+    info_pd.channel.release_pkt = nullptr;
+    info_pd.channel.close = nullptr;
+    info_pd.scbk = nullptr;
+}
 
 int pd_command_handler(void *data, struct osdp_cmd *cmd)
 {
@@ -86,6 +80,7 @@ void setup()
 
     pd.logger_init("osdp::pd", OSDP_LOG_DEBUG, NULL);
 
+    init_pd_info();
     pd.setup(&info_pd);
 
     pd.set_command_callback(pd_command_handler, nullptr);

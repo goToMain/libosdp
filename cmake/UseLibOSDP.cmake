6c8b0c5ea98212e1efa938d0b5b50414d48b0a35
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2026-04-26 08:17:03 +0530

    cmake: Make find_package(libosdp) work for all crypto backends
    The installed libosdpConfig.cmake used a brittle if(@OpenSSL_FOUND@)
    substitution that relied on an explicit defensive set(... FALSE) in the
    build to avoid emitting if(). Replace it with a configure-time-composed
    @LIBOSDP_FIND_DEPENDENCY_BLOCK@. When MbedTLS is selected, also install
    FindMbedTLS.cmake alongside the package config so downstream
    find_dependency(MbedTLS) resolves.
    
    Add a libosdp::libosdp install-tree alias that resolves to the shared
    library when present, falling back to static. Update examples to use
    the canonical alias instead of the build-tree-only TARGET_EXISTS genex.
    
    Drop UseLibOSDP.cmake (dead, referenced undefined LIBOSDP_* vars).
    
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- cmake/UseLibOSDP.cmake
+++
@@ -1,9 +0,0 @@
-#
-#  Copyright (c) 2020-2026 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
-#
-#  SPDX-License-Identifier: Apache-2.0
-#
-
-add_definitions(${LIBOSDP_DEFINITIONS})
-include_directories(${LIBOSDP_INCLUDE_DIR})
-link_directories(${LIBOSDP_LIBRARY_DIR})
